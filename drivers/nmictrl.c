2b2763de6b643ca50adedd4bdb8c3d4a649ceeb3
Hyeonho Seo <seohho@gmail.com>
2019-04-09 18:56:15 +0900

    indev-190409-01: Documentate nmictrl & Fix some bugs
    Drivers:
        - MODIFIED: drivers/nmictrl/
    	* Add doxygen formatted comments for auto-documentation
    	* Fix some preemption related risky codes

--- drivers/nmictrl.c
+++ drivers/nmictrl.c
@@ -1,3 +1,13 @@
+/**
+ * @file nmictrl.c
+ * @brief The NMI control subsystem.
+ *
+ * This is implementations of 'NMI control subsystem'
+ *
+ * @author Hyeonho Seo (revimal)
+ * @bugs No Known Bugs
+ */
+
 #include "nmictrl.h"
 
 #include <asm/apic.h>
@@ -14,12 +24,27 @@
 
 #define NMICTRL_GENERIC_HANDLER_NAME "nmictrl_generic_handler"
 
+/**
+ * @brief User-defined handler structure.
+ */
+typedef struct {
+	char handler_name[NMICTRL_HANDLER_NAMESZ];
+	cpumask_t handler_mask;
+	nmictrl_fn_t handler_fn;
+	struct list_head handler_list;
+	struct rcu_head handler_rcu;
+} nmictrl_handler_t;
+
 static cpumask_t nmictrl_processor_mask;
 
 static DEFINE_SPINLOCK(nmictrl_global_write_lock);
 static LIST_HEAD(nmictrl_handler_list);
 
-/* RCU protected */
+/**
+ * @brief Wrapper function to handle generated IPI signal.
+ *
+ * @p cmd will not pass to the user-registered handler, because it always is 'NMI_LOCAL'.
+ */
 static int nmictrl_generic_handler(unsigned int cmd, struct pt_regs *regs)
 {
 	nmictrl_ret_t handler_ret;
@@ -28,6 +53,41 @@ static int nmictrl_generic_handler(unsigned int cmd, struct pt_regs *regs)
 	if (cpumask_test_and_clear_cpu(smp_processor_id(), &nmictrl_processor_mask))
 	{
 		rcu_read_lock();
+		/*
+		 * This is "Non-maskable" intetrupt handler callback.
+		 * It means, your CPU can enter this routine even it in IRQ disabled state.
+		 * In that case, we cannot guarantee serialized synchronization using normal RCU APIs on the SMP system.
+		 *
+		 * Following example illustrate calling 'nmictrl_shutdown_sync' in CPU1 at the same time with NMI self-triggered in CPU0.
+		 * If we use normal 'list_for_each_entry_rcu' to iterate prepared handler, a horrible disaster will strike on our system.
+		 *
+		 * INITIAL LIST STATE:
+		 *  [0]: handler_0
+		 *  [1]: handler_1 -> Will be executed
+		 *  [2]: handler_2
+		 *
+		 * CPU0 							 CPU1
+		 * ---- 							 ----
+		 *      							 CALL_SHUTDOWN_SYNC
+		 * TRIGGER_LIST_ENTRY[1]
+		 *      							 ACQUIRE_IRQ_DISABLE
+		 * ENTER_NMI_CONTEXT
+		 * ACCESS_LIST_ENTRY[0]
+		 *      							 REMOVE_LIST_ENTRY[0]
+		 * LIST_FOREACH_BREAK
+		 * NOT_CLEANED_CPUMASK[1]
+		 *      							 REMOVE_LIST_ENTRY[1]
+		 *      							 REMOVE_LIST_ENTRY[2]
+		 *      							 WAIT_FOR_CPUMASK_EMPTY
+		 * ALL_LIST_ENTRY_REMOVED
+		 * IPI_CANNOT_BE_TRIGGERED
+		 * SO_CPUMASK_CANNOT_BE_EMPTY
+		 *      							 STILL_WATINIG_FOR_CPUMASK_EMPTY
+		 *      							 ...WTF?????????????????
+		 *
+		 * This is why I implemented 'list_for_each_entry_safe_rcu'.
+		 * Seems legit? :P
+		 */
 		list_for_each_entry_safe_rcu(handler_ptr, handler_nptr, &nmictrl_handler_list, handler_list) {
 			if (cpumask_test_and_clear_cpu(smp_processor_id(), &handler_ptr->handler_mask)) {
 				nmictrl_fn_t handler_fn;
@@ -47,6 +107,12 @@ static int nmictrl_generic_handler(unsigned int cmd, struct pt_regs *regs)
 	return NMI_HANDLED;
 }
 
+/**
+ * @brief Internal function to reclaim a user-registered handler when rcu grace period expired.
+ *
+ * It always passes to 'call_rcu' kernel function; do not call this function directly.
+ * #handler_rcu will automatically be filled by kernel rcu reclaimer callee.
+ */
 static void nmictrl_reclaim_handler(struct rcu_head *handler_rcu)
 {
 	nmictrl_handler_t *handler_ptr =
@@ -58,6 +124,9 @@ static void nmictrl_reclaim_handler(struct rcu_head *handler_rcu)
 	kfree(handler_ptr);
 }
 
+/**
+ * @brief internal function to flush-out all user-registered handlers.
+ */
 static void nmictrl_clear_handler_unlocked(void)
 {
 	nmictrl_handler_t *handler_ptr, *handler_nptr;
@@ -71,38 +140,62 @@ static void nmictrl_clear_handler_unlocked(void)
 int nmictrl_init(void)
 {
 	int ret;
-	unsigned long irqflag;
 
-	spin_lock_irqsave(&nmictrl_global_write_lock, irqflag);
+	spin_lock(&nmictrl_global_write_lock);
 	ret = register_nmi_handler(NMI_LOCAL, nmictrl_generic_handler, 0, NMICTRL_GENERIC_HANDLER_NAME);
 	wmb();
-	spin_unlock_irqrestore(&nmictrl_global_write_lock, irqflag);
+	spin_unlock(&nmictrl_global_write_lock);
 	pr_info("Successfully attached nmictrl-subsystem\n");
 	return ret;
 }
 
 void nmictrl_shutdown(void)
 {
-	unsigned long irqflag;
-
-	spin_lock_irqsave(&nmictrl_global_write_lock, irqflag);
+	spin_lock(&nmictrl_global_write_lock);
 	nmictrl_clear_handler_unlocked();
+	/*
+	 * Put memory barrier here to prevent overlapping between cpumask_clear code and new IPI signal.
+	 *
+	 * All 'nmictrl_trigger_*' functions check handler list existence before they raise IPI signal.
+	 * Thus, when all handler list flushed-out, no more IPI signal will generate.
+	 *
+	 * This memory barrier guaranteeing all handler flushed-out before we clear the cpumask.
+	 */
 	smp_wmb();
 	cpumask_clear(&nmictrl_processor_mask);
 	smp_wmb();
 	unregister_nmi_handler(NMI_LOCAL, NMICTRL_GENERIC_HANDLER_NAME);
-	spin_unlock_irqrestore(&nmictrl_global_write_lock, irqflag);
+	wmb();
+	spin_unlock(&nmictrl_global_write_lock);
 	pr_info("Successfully detached nmictrl-subsystem\n");
 }
 
 void nmictrl_shutdown_sync(void)
 {
+	/*
+	 * Try to trigger all prepared handlers.
+	 */
+	nmictrl_trigger_all();
 	spin_lock(&nmictrl_global_write_lock);
 	nmictrl_clear_handler_unlocked();
+	smp_wmb();
+	/*
+	 * NMI-Enter detecting phase.
+	 * If 'nmictrl_processor_mask' is not empty, meaning triggered but not yet handled IPI signal still exist.
+	 * An each bit of 'nmictrl_processor_mask' is cleared when IPI signal handled and entered NMI context.
+	 * To sum up, for detecting that all IPI signals handled, we must observe the cpumask is empty.
+	 */
 	while(!cpumask_empty(&nmictrl_processor_mask))
 		cpu_relax();
+	/*
+	 * NMI-Exit detecting phase.
+	 * 'nmictrl_generic_handler' always return with rcu_read_unlock.
+	 * Therefore, we can determine that other CPUs escaped from NMI context by detecting the RCU grace period.
+	 */
 	rcu_barrier();
-	smp_wmb();
+	/*
+	 * No unhandled NMI. Hooray!
+	 */
 	unregister_nmi_handler(NMI_LOCAL, NMICTRL_GENERIC_HANDLER_NAME);
 	wmb();
 	spin_unlock(&nmictrl_global_write_lock);
@@ -167,25 +260,21 @@ skip_unlock:;
 
 int nmictrl_add_handler(const char *handler_name, nmictrl_fn_t handler_fn)
 {
-	int ret;
 	nmictrl_handler_t *handler_ptr;
 
 	spin_lock(&nmictrl_global_write_lock);
 	list_for_each_entry(handler_ptr, &nmictrl_handler_list, handler_list) {
 		if (strncmp(handler_ptr->handler_name, handler_name, NMICTRL_HANDLER_NAMESZ) == 0) {
-			ret = NMICTRL_ERROR;
 			goto error;
 		}
 	}
 
 	handler_ptr = kzalloc(sizeof(*handler_ptr), GFP_ATOMIC);
 	if ( handler_ptr == NULL ) {
-		ret = NMICTRL_ERROR;
 		goto error;
 	}
 
 	if ( handler_fn == NULL ) {
-		ret = NMICTRL_ERROR;
 		goto error;
 	}
 
@@ -193,7 +282,6 @@ int nmictrl_add_handler(const char *handler_name, nmictrl_fn_t handler_fn)
 		strlen(handler_name) >= NMICTRL_HANDLER_NAMESZ ||
 		strlen(handler_name) !=
 			strlcpy(handler_ptr->handler_name, handler_name, NMICTRL_HANDLER_NAMESZ)) {
-		ret = NMICTRL_ERROR;
 		goto error;
 	}
 	handler_ptr->handler_fn = handler_fn;
@@ -205,14 +293,14 @@ int nmictrl_add_handler(const char *handler_name, nmictrl_fn_t handler_fn)
 	spin_unlock(&nmictrl_global_write_lock);
 	pr_info("Successfully registered nmi_handler(%p:%s:%p)\n",
 		handler_ptr, handler_ptr->handler_name, handler_ptr->handler_fn);
-	return ret;
+	return NMICTRL_SUCCESS;
 
 error:
 	spin_unlock(&nmictrl_global_write_lock);
 	pr_warn("Failed to register nmi_handler(%p:%s->%s:%p)\n",
 		handler_ptr, !!(handler_name) ? handler_name : "NULL",
 		!!(handler_ptr) ? handler_ptr->handler_name : "NULL", (void *)handler_fn);
-	return ret;
+	return NMICTRL_ERROR;
 }
 
 void nmictrl_del_handler(const char *handler_name)
--- drivers/nmictrl.h
+++ drivers/nmictrl.h
@@ -1,3 +1,14 @@
+/**
+ * @file nmictrl.h
+ * @brief Prototypes for 'NMI control subsystem'.
+ *
+ * This contains the function prototypes, macros,
+ * structures, enums, etc. for 'NMI control subsystem'
+ *
+ * @author Hyeonho Seo (revimal)
+ * @bugs No Known Bugs
+ */
+
 #ifndef _NMDBG_NMICTRL_H
 #define _NMDBG_NMICTRL_H
 
@@ -7,35 +18,95 @@
 #include "define.h"
 
 #define NMICTRL_HANDLER_NAMESZ 32
+#define NMICTRL_SUCCESS NMICTRL_HANDLED
 
+/**
+ * @brief Return values for NMI control subsystem.
+ */
 typedef enum {
-	NMICTRL_HANDLED,
-	NMICTRL_FORWARD,
+	NMICTRL_HANDLED, /** As same as #NMICTRL_SUCCESS */
 	NMICTRL_ERROR,
+	NMICTRL_FORWARD,
 } nmictrl_ret_t;
 
+/**
+ * @brief User-defined handler function type.
+ */
 typedef nmictrl_ret_t (*nmictrl_fn_t)(struct pt_regs *);
 
-typedef struct {
-	char handler_name[NMICTRL_HANDLER_NAMESZ];
-	cpumask_t handler_mask;
-	nmictrl_fn_t handler_fn;
-	struct list_head handler_list;
-	struct rcu_head handler_rcu;
-} nmictrl_handler_t;
-
+/**
+ * @brief Activate the NMI control subsystem.
+ * @return
+ * 	0 if initialization success.
+ */
 int nmictrl_init(void);
+
+/**
+ * @brief deactivate the NMI control subsystem.
+ */
 void nmictrl_shutdown(void);
+
+/**
+ * @brief Deactivate NMI control subsystem and wait for triggered IPI signals to finish.
+ */
 void nmictrl_shutdown_sync(void);
 
+/**
+ * @brief Send IPI signals to entire CPUs.
+ */
 void nmictrl_trigger_all(void);
+
+/**
+ * @brief Send IPI signals to the current CPU.
+ */
 void nmictrl_trigger_self(void);
+
+/**
+ * @brief Send IPI signals to all the other CPUs.
+ */
 void nmictrl_trigger_others(void);
+
+/**
+ * @brief Send IPI signals to the specific CPUs.
+ */
 void nmictrl_trigger_cpu(unsigned int cpu_id);
 
+/**
+ * @brief Register a user-defined handler.
+ *
+ * Length of @p handler_name must be shorter than 32 characters.
+ *
+ * @param handler_name
+ * 	The handler name to be registered
+ * @param handler_fn
+ * 	The handler callback to be registered
+ * @return
+ * 	0(NMICTRL_SUCCESS) upon successful add the handler
+ */
 int nmictrl_add_handler(const char *handler_name, nmictrl_fn_t nmi_handler);
+
+/**
+ * @brief Unregister a user-defined handler.
+ * @param handler_name
+ * 	The handler name to be unregistered
+ */
 void nmictrl_del_handler(const char *handler_name);
+
+/**
+ * @brief Unregister all user-defined handlers.
+ */
 void nmictrl_clear_handler(void);
 
+/**
+ * @brief Prepare a user-defined handler.
+ *
+ * "prepare" means notify to the #nmictrl_generic_handler that there are some handlers must be executed in.
+ * The generic handler instantly returns without any actions if no any "prepared" user-handlers.
+ *
+ * @param handler_name
+ * 	The handler name to be prepared
+ * @param cpu_id
+ * 	The cpu id that handler will be triggered on
+ */
 void nmictrl_prepare_handler(const char *handler_name, unsigned int cpu_id);
 #endif
